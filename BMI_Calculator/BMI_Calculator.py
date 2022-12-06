ab3279d39638e9a1cb7a55531dffc4645bec65b2
Ademba <oscarademba+github@gmail.com>
2022-12-06 11:04:35 +0300

    Create BMI_Calculator.py

---
+++ BMI_Calculator/BMI_Calculator.py
@@ -0,0 +1,19 @@
+### BMI Calculator
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+
+BMI = float (weight) /(float (height) ** 2)
+
+print(int(BMI))
+
+#f-string
+score = 1
+height = 1.7
+isWinning = True
+
+print(f"Your score is {score}, your height is {height} and your winning is {isWinning}")
+
+
+
